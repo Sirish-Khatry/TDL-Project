713d40b65f5f0d8c3b4b9a1313bfacc342e1dc39
Sirish-Khatry <SKhatry@qa.com>
2021-08-03 17:18:54 +0100

    First view feature commit

--- java/src/main/java/com/qa/tdl/entity/Item.java
+++ java/src/main/java/com/qa/tdl/entity/Item.java
@@ -1,6 +1,58 @@
 package com.qa.tdl.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
+@Entity
+@Table(name = "item")
 public class Item {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@NotNull
+	@Column(unique = true)
+	private String task;
+	@NotNull
+	private String status;
+
+	public Item() {
+		super();
+	}
+
+	public Item(int id, String task, String status) {
+		super();
+		this.id = id;
+		this.task = task;
+		this.status = status;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTask() {
+		return task;
+	}
+
+	public void setTask(String task) {
+		this.task = task;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
 }
